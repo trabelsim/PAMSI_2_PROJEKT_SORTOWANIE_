13fc6bcf2ce378c9758c7b9e813be1f6dc0e1813
Maroine <238966@student.pwr.edu.pl>
2018-04-25 21:30:24 +0200

    Dodano opisy i szczegoly utworzonych zmiennych pomocniczych

--- Shell.cpp
+++ Shell.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
-#define rozmiar 100
+#define rozmiar 100                             // Rozmiar tablicy ktora nalezy posortowac
 using namespace std;
 
-void zbudujShell (int liczba, int n[])
+void zbudujShell (int liczba, int n[])          // Funkcja sluzy do Wprowadzania liczb do tablicy
 {
 
     for(int i=0;i<liczba;i++)
     {
-        cout<<"Wprowadz liczbe"<<i+1<<" : ";
-        cin>>n[i];
+        cout<<"Wprowadz liczbe"<<i+1<<" : ";    // Wyswietla kolejno ilosc wszystkich liczb od 0+1 do maksymalnej wartosci (liczba)
+        cin>>n[i];                              // Wprowadza do tablicy kolejno liczby
     }
 }
 
-void PokazShell(int liczba, int n[])
+void PokazShell(int liczba, int n[])            // Funkcja sluzy do wyswietlania kolejno liczby z tablicy, przy 2 argumentach - tablica oraz miejsce.
 {
     for(int i=0;i<liczba;i++)
-        cout<<n[i]<<endl;
+        cout<<n[i]<<endl;                       //Wyswietla kolejno liczby.
 
 }
 
-void SortujShell(int a[], int ilosc)
+void SortujShell(int a[], int ilosc)            // Funkcja sluzy do sortowania tablicy wedlug Shella
 {
-    int i,j , inc, temp;
+    int i,j , inc, temp;                        // i
 
-    for(inc = 1; inc < ilosc ; inc = inc*3+1);
+    for(inc = 1; inc < ilosc ; inc = inc*3+1);  // dla wartosci inc =1 mnoz razy 3 i dodaj 1 az osiagnie wartosc ilosci.
 
-    while(inc>0)
+    while(inc>0)                                // Pierwszy warunek - zmienna inc bedzie zawsze wieksza od 0.
     {
-        for(i=inc;i<ilosc;i++)
+        for(i=inc;i<ilosc;i++)                  // Przypisywanie wartosci inc do zmiennej i i zwiekszanie ja do poki bedzie mniejsza od ilosc.
         {
-            j=i;
-            temp = a[i];
-            while((j>= inc)&&(a[j-inc]>temp))
+            j=i;                                // Przypisanie wartosci i do zmiennej j
+            temp = a[i];                        // Przechowanie tablicy zaleznej od zmiennej i do temp.
+            while((j>= inc)&&(a[j-inc]>temp))   // Kolejny warunek bedzie sie spelnial poki zmienna j bedzie wieksza od inc a zarazem tablica a[i] mniejsza od tablicy a[j-inc]
             {
-                a[j] = a[j - inc];
-                j=j-inc;
+                a[j] = a[j - inc];              // Przepisana zostaje wartosc drugiej tablicy do a[j]
+                j=j-inc;                        // Nastepnie j sie zmniejsza za razem o inc.
             }
-            a[j] = temp;
+            a[j] = temp;                        // Po wykonaniu 2-ego warunku tablica a[j] przyjmuje wartosc temp.
         }
-        inc/=2;
+        inc/=2;                                 // A wartosc inc zostaje podzielona przez dwa.
     }
 }
 
